ce9f2dd98792762fd07086270ce38e0be199e5f0
LeoCavalcante07 <leonardo_cavalcante.07@hotmail.com>
2025-01-18 17:11:44 +0000

    cf api permission

---
+++ gcp_api_permissions.tf
@@ -0,0 +1,8 @@
+provider "google" {
+  project = "moonlit-bliss-448020-r3"    # Substitua pelo ID do seu projeto
+  region  = "US"       # Substitua pela região desejada
+}
+
+resource "google_project_service" "cloud_functions" {
+  service = "cloudfunctions.googleapis.com"
+}
