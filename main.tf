34c755d9e52794cbf9c98070cb25e67c1ed21e2d
LeoCavalcante07 <leonardo_cavalcante.07@hotmail.com>
2025-01-18 16:55:10 +0000

    2 obj same file test

--- main.tf
+++ main.tf
@@ -1,6 +1,15 @@
-resource "google_storage_bucket" "my-bucket-1801_2" {
+resource "google_storage_bucket" "my-bucket-1801_3" {
   project       = "moonlit-bliss-448020-r3"
-  name          = "my-bucket-1801_2"
+  name          = "my-bucket-1801_3"
+  location      = "US"
+  force_destroy = true
+
+  public_access_prevention = "enforced"
+}
+
+resource "google_storage_bucket" "my-bucket-1801_4" {
+  project       = "moonlit-bliss-448020-r3"
+  name          = "my-bucket-1801_4"
   location      = "US"
   force_destroy = true
 
--- second.tf
+++
@@ -1,8 +0,0 @@
-resource "google_storage_bucket" "my-bucket-1801" {
-  project       = "moonlit-bliss-448020-r3"
-  name          = "my-bucket-1801"
-  location      = "US"
-  force_destroy = true
-
-  public_access_prevention = "enforced"
-}
