22b969328b5fea0bbb2fa799d1b79063fc2636a1
LeoCavalcante07 <leonardo_cavalcante.07@hotmail.com>
2025-01-18 16:50:49 +0000

    second tf file test

--- second.tf
+++ second.tf
@@ -1,6 +1,6 @@
-resource "google_storage_bucket" "my-bucket-1801_2" {
+resource "google_storage_bucket" "my-bucket-1801" {
   project       = "moonlit-bliss-448020-r3"
-  name          = "my-bucket-1801_2"
+  name          = "my-bucket-1801"
   location      = "US"
   force_destroy = true
 
