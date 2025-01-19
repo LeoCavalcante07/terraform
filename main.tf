ae4893cbd8654cddf446680213cfabaff0535f89
LeoCavalcante07 <leonardo_cavalcante.07@hotmail.com>
2025-01-19 19:47:42 +0000

    my first cf using tf

--- main.tf
+++ main.tf
@@ -1,17 +1,35 @@
-resource "google_storage_bucket" "my-bucket-1801_3" {
+resource "google_storage_bucket" "codes_bucket" {
   project       = "moonlit-bliss-448020-r3"
-  name          = "my-bucket-1801_3"
-  location      = "US"
+  name     = "codes_bucket"
+  location = "US"
   force_destroy = true
-
   public_access_prevention = "enforced"
 }
 
-resource "google_storage_bucket" "my-bucket-1801_4" {
-  project       = "moonlit-bliss-448020-r3"
-  name          = "my-bucket-1801_4"
-  location      = "US"
-  force_destroy = true
+resource "google_storage_bucket_object" "source_code" {
+  name   = "index.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "index.zip"
+}
 
-  public_access_prevention = "enforced"
+resource "google_cloudfunctions_function" "func_1_test" {
+  name        = "func_1_test"
+  description = "My function TF"
+  runtime     = "python39"
+
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive.name
+  trigger_http          = true
+  entry_point           = "hello_http"
+}
+
+# IAM entry for all users to invoke the function
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.function.project
+  region         = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
 }
