3e32256d87a938e96251f0ba78a362f487340961
LeoCavalcante07 <leonardo_cavalcante.07@hotmail.com>
2025-01-20 12:33:22 +0000

    terraform config in one single file

--- gcp_api_permissions.tf
+++
@@ -1,9 +0,0 @@
-
-
-resource "google_project_service" "required_apis" {
-  for_each = toset([
-    "cloudfunctions.googleapis.com",
-    "cloudbuild.googleapis.com"
-  ])
-  service = each.key
-}
--- main.tf
+++ main.tf
@@ -3,6 +3,14 @@ provider "google" {
   region  = "US"                      # Substitua pela região desejada
 }
 
+resource "google_project_service" "required_apis" {
+  for_each = toset([
+    "cloudfunctions.googleapis.com",
+    "cloudbuild.googleapis.com"
+  ])
+  service = each.key
+}
+
 terraform {
   backend "gcs" {
     bucket = "terraform-config-1801"     # Nome do bucket GCS
