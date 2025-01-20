19bcc11d7193e8a30778b34eb04a855c08e1672c
LeoCavalcante07 <leonardo_cavalcante.07@hotmail.com>
2025-01-20 14:25:57 +0000

    removing cf test

--- main.tf
+++ main.tf
@@ -33,24 +33,24 @@ resource "google_storage_bucket_object" "source_code" {
   source = "function_code.zip"
 }
 
-resource "google_cloudfunctions_function" "func_1_test" {
-  name                  = "func_1_test"
-  description           = "My function TF"
-  runtime               = "python39"
-  region                = "us-central1"
-  available_memory_mb   = 256
-  source_archive_bucket = google_storage_bucket.codes_bucket_1901.name
-  source_archive_object = google_storage_bucket_object.source_code.name
-  trigger_http          = true
-  entry_point           = "hello_http"
-}
+# resource "google_cloudfunctions_function" "func_1_test" {
+#   name                  = "func_1_test"
+#   description           = "My function TF"
+#   runtime               = "python39"
+#   region                = "us-central1"
+#   available_memory_mb   = 256
+#   source_archive_bucket = google_storage_bucket.codes_bucket_1901.name
+#   source_archive_object = google_storage_bucket_object.source_code.name
+#   trigger_http          = true
+#   entry_point           = "hello_http"
+# }
 
-# IAM entry for all users to invoke the function
-resource "google_cloudfunctions_function_iam_member" "invoker" {
-  project        = google_cloudfunctions_function.func_1_test.project
-  region         = google_cloudfunctions_function.func_1_test.region
-  cloud_function = google_cloudfunctions_function.func_1_test.name
+# # IAM entry for all users to invoke the function
+# resource "google_cloudfunctions_function_iam_member" "invoker" {
+#   project        = google_cloudfunctions_function.func_1_test.project
+#   region         = google_cloudfunctions_function.func_1_test.region
+#   cloud_function = google_cloudfunctions_function.func_1_test.name
 
-  role   = "roles/cloudfunctions.invoker"
-  member = "allUsers"
-}
+#   role   = "roles/cloudfunctions.invoker"
+#   member = "allUsers"
+# }
