ab845c4720caac1df8dd930fcd7a8c43df0d1994
LeoCavalcante07 <leonardo_cavalcante.07@hotmail.com>
2025-01-20 13:56:06 +0000

    deleting api part

--- main.tf
+++ main.tf
@@ -3,13 +3,13 @@ provider "google" {
   region  = "US"                      # Substitua pela região desejada
 }
 
-# resource "google_project_service" "required_apis" {
-#   for_each = toset([
-#     "cloudfunctions.googleapis.com",
-#     "cloudbuild.googleapis.com"
-#   ])
-#   service = each.key
-# }
+resource "google_project_service" "required_apis" {
+  for_each = toset([
+    "cloudfunctions.googleapis.com",
+    "cloudbuild.googleapis.com"
+  ])
+  service = each.key
+}
 
 terraform {
   backend "gcs" {
